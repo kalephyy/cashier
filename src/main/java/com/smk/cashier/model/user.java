57cfd37318c51510d2f052f74f78e6aeb1fcc506
Kalipi <official.farelindratno@gmail.com>
2023-08-30 14:03:35 +0700

    cashier

--- src/main/java/com/smk/cashier/model/user.java
+++ src/main/java/com/smk/cashier/model/user.java
@@ -1,7 +1,7 @@
 package com.smk.cashier.model;
 
 public class user extends model {
-    private String userName;
+    private String userName
     private String password;
 
     public String getUsername() {
